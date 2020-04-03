66755d7b41acaee4d47f79d5e3c5bdaae47e475b
nyaruko <shupenglai@gmail.com>
2020-04-04 04:16:49 +0800

    add in the map

--- include/cpc_motion_planning/pso/pso_dynamics.cuh
+++ include/cpc_motion_planning/pso/pso_dynamics.cuh
@@ -68,15 +68,61 @@ float3 dp_control(const State &s, const float3 &site, VoidPtrCarrier<N> &aux_dat
 __host__ __device__ __forceinline__
 float process_cost(const State &s, const State &goal, const EDTMap &map)
 {
+  float cost = 0;
   float2 dist_err = s.p - goal.p;
-  return 0.5f*sqrt(dist_err.x*dist_err.x + dist_err.y*dist_err.y) + 0.2f*sqrt(s.v*s.v + s.w*s.w);
+  cost += 0.5f*sqrt(dist_err.x*dist_err.x + dist_err.y*dist_err.y) + 0.2f*sqrt(s.v*s.v + s.w*s.w);
+
+  int ix = floor( (s.p.x - map.m_origin.x) / map.m_grid_step + 0.5);
+  int iy = floor( (s.p.y - map.m_origin.y) / map.m_grid_step + 0.5);
+  int iz = floor( (0.35 - map.m_origin.z) / map.m_grid_step + 0.5);
+
+  if (ix<0 || ix>=map.m_map_size.x ||
+          iy<0 || iy>=map.m_map_size.y ||
+          iz<0 || iz>=map.m_map_size.z)
+  {
+      cost += 100;
+      return cost;
+  }
+
+#ifdef  __CUDA_ARCH__
+  float rd = map.edt_const_at(ix,iy,iz).d*0.2;
+  cost += exp(-6*rd)*400;
+
+  if (rd < 0.8)
+    cost += 100;
+#endif
+
+  return  cost;
 }
 
 __host__ __device__ __forceinline__
 float final_cost(const State &s, const State &goal, const EDTMap &map)
 {
+  float cost = 0;
   float2 dist_err = s.p - goal.p;
-  return 4.0f*sqrt(dist_err.x*dist_err.x + dist_err.y*dist_err.y) + 0.2f*sqrt(s.v*s.v + s.w*s.w);
+  cost += 4.0f*sqrt(dist_err.x*dist_err.x + dist_err.y*dist_err.y) + 0.2f*sqrt(s.v*s.v + s.w*s.w);
+
+  int ix = floor( (s.p.x - map.m_origin.x) / map.m_grid_step + 0.5);
+  int iy = floor( (s.p.y - map.m_origin.y) / map.m_grid_step + 0.5);
+  int iz = floor( (0.35 - map.m_origin.z) / map.m_grid_step + 0.5);
+
+  if (ix<0 || ix>=map.m_map_size.x ||
+          iy<0 || iy>=map.m_map_size.y ||
+          iz<0 || iz>=map.m_map_size.z)
+  {
+      cost += 100;
+      return cost;
+  }
+
+#ifdef  __CUDA_ARCH__
+  float rd = map.edt_const_at(ix,iy,iz).d*0.2;
+  cost += exp(-6*rd)*400;
+
+  if (rd < 0.8)
+    cost += 100;
+#endif
+
+  return  cost;
 }
 
 //---
