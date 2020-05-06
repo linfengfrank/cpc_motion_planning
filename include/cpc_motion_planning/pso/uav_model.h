d6aa1a3a766bfb1f63ebb7373354cc193139462f
nyaruko <shupenglai@gmail.com>
2020-05-06 18:22:36 +0800

    prepare the uav_model class

--- include/cpc_motion_planning/pso/uav_model.h
+++ include/cpc_motion_planning/pso/uav_model.h
@@ -6,49 +6,18 @@ namespace PSO
 class UAVModel
 {
 public:
-  struct State
-  {
-    float3 p;
-    float3 v;
-    float3 a;
-    __host__ __device__
-    State():
-      p(make_float3(0,0,0)),
-      v(make_float3(0,0,0)),
-      a(make_float3(0,0,0))
-    {
-    }
-  };
+UAVModel()
+{
 
-  struct Target
-  {
-    State s;
-    bool oa;
-  };
+}
+
+~UAVModel()
+{
+
+}
 
 
-  __host__ __device__ __forceinline__
-  void model_forward(State &s, const float3 &u, const float &dt)
-  {
-    // x
-    s.p.x = s.p.x + s.v.x*dt + 0.5f*s.a.x*dt*dt + 1.0f/6.0f*u.x*dt*dt*dt;
-    s.v.x = s.v.x + s.a.x*dt + 0.5f*u.x*dt*dt;
-    s.a.x = s.a.x + u.x*dt;
-
-    // y
-    s.p.y = s.p.y + s.v.y*dt + 0.5f*s.a.y*dt*dt + 1.0f/6.0f*u.y*dt*dt*dt;
-    s.v.y = s.v.y + s.a.y*dt + 0.5f*u.y*dt*dt;
-    s.a.y = s.a.y + u.y*dt;
-
-    // z
-    s.p.z = s.p.z + s.v.z*dt + 0.5f*s.a.z*dt*dt + 1.0f/6.0f*u.z*dt*dt*dt;
-    s.v.z = s.v.z + s.a.z*dt + 0.5f*u.z*dt*dt;
-    s.a.z = s.a.z + u.z*dt;
-
-
-  }
 };
-
 }
 
 #endif // UAV_MODEL_H
