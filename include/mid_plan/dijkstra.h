f17cc02ad60b7cc34727a08b8abeeadca192faae
nyaruko <shupenglai@gmail.com>
2020-06-01 14:34:58 +0800

    add 3d nf1 calculation

--- include/mid_plan/dijkstra.h
+++ include/mid_plan/dijkstra.h
@@ -8,6 +8,7 @@ class Dijkstra : public GridGraph
 public:
   Dijkstra(int maxX, int maxY, int maxZ);
   void dijkstra2D(CUDA_GEO::coord glb_tgt);
+  void dijkstra3D(CUDA_GEO::coord glb_tgt);
 };
 
 #endif // DIJKSTRA_H
--- src/mid_plan/dijkstra.cpp
+++ src/mid_plan/dijkstra.cpp
@@ -57,3 +57,58 @@ void Dijkstra::dijkstra2D(CUDA_GEO::coord glb_tgt)
     }
   }
 }
+
+void Dijkstra::dijkstra3D(CUDA_GEO::coord glb_tgt)
+{
+  memcpy(_id_map,_init_id_map,sizeof(nodeInfo)*static_cast<size_t>(_w*_h*_d));
+  //Get the local Dijkstra target
+  CUDA_GEO::coord mc = glb_tgt;
+  CUDA_GEO::coord pc;
+  nodeInfo* m;
+  // insert the root
+  m=getNode(mc);
+
+  bool occupied=false;
+  if (m)
+  {
+    m->g = 0 + obsCostAt(mc,0,occupied);
+    _PQ.insert(m,m->g);
+  }
+  while (_PQ.size()>0)
+  {
+    m=_PQ.pop();
+    m->inClosed = true;
+    mc = m->c;
+
+    // get all neighbours
+    for (int ix=-1;ix<=1;ix++)
+    {
+      for (int iy=-1;iy<=1;iy++)
+      {
+        for (int iz=-1;iz<=1;iz++)
+        {
+          if (ix==0 && iy ==0 && iz == 0)
+            continue;
+
+          pc.x = mc.x + ix;
+          pc.y = mc.y + iy;
+          pc.z = mc.z + iz;
+          nodeInfo* p = getNode(pc);
+          if (p)
+          {
+            if (!p->inClosed)
+            {
+              float new_g = sqrtf(static_cast<float>(ix*ix+iy*iy+iz*iz))*getGridStep() +
+                  m->g + obsCostAt(pc,0,occupied);
+              if (p->g > new_g)
+              {
+                p->g = new_g;
+                _PQ.insert(p,p->g);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
