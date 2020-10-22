ac383bca0ff0d482b9b81677ed0e31858cfb901d
nyaruko <shupenglai@gmail.com>
2020-10-22 12:29:50 +0800

    the 2d version use higher resolution

--- src/mid_plan/line_follow_mid_planner_node.cpp
+++ src/mid_plan/line_follow_mid_planner_node.cpp
@@ -362,7 +362,7 @@ int main(int argc, char **argv)
   mid_goal_pub = new ros::Publisher;
   glb_goal_pub = new ros::Publisher;
   ros::NodeHandle nh;
-  ros::Subscriber map_sub = nh.subscribe("/lowres_map", 1, &mapCallback);
+  ros::Subscriber map_sub = nh.subscribe("/edt_map", 1, &mapCallback);
   ros::Subscriber stuck_sub = nh.subscribe("/stuck", 1, &stuckCallback);
   ros::Subscriber mission_sub = nh.subscribe("/start_mission", 1, &load_mission_callback);
 
