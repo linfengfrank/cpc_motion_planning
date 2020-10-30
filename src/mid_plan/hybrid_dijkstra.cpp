756e73dcedbc8ec12f90c16c0c91e0902231fa8a
nyaruko <shupenglai@gmail.com>
2020-10-30 15:04:22 +0800

    update the hybrid planning for test

--- src/mid_plan/hybrid_dijkstra.cpp
+++ src/mid_plan/hybrid_dijkstra.cpp
@@ -172,6 +172,80 @@ void Dijkstra::hybrid_update_neighbour(const float3 &shift_pose, nodeInfo *m, fl
   }
 }
 
+CUDA_GEO::coord  Dijkstra::hybrid_bfs(const float3 &start_pose, CUDA_GEO::coord glb_tgt)
+{
+  //First clear the old information
+  memcpy(m_id_map,m_init_id_map,sizeof(nodeInfo)*static_cast<size_t>(_w*_h*THETA_GRID_SIZE));
+  _PQ.clear();
+
+  // Useful variables
+  CUDA_GEO::coord closest_coord,start;
+  float min_h = std::numeric_limits<float>::infinity();
+
+  //Set the root
+  start = float32coord(start_pose);
+  nodeInfo* ni=m_getNode(start);
+  if (ni)
+  {
+    ni->g = 0;
+    ni->h = dist2D(ni->c,glb_tgt);
+    ni->pose = start_pose;
+    _PQ.insert(ni,ni->g);
+    closest_coord = ni->c;
+    min_h = ni->h;
+  }
+  else
+  {
+    ROS_ERROR("First point outside the boundary.");
+    exit(-1);
+  }
+
+  // start the hybrid D
+  bool reached_free_zone = false;
+  while (_PQ.size()>0)
+  {
+//    std::cout<<ni->c.x<<" "<<ni->c.y<<std::endl;
+//    std::cout<<"----"<<std::endl;
+    ni=_PQ.pop();
+    ni->inClosed = true;
+
+    if(mm_isfree(ni->c))
+      reached_free_zone = true;
+
+    if (min_h > ni->h)
+    {
+      min_h = ni->h;
+      closest_coord = ni->c;
+    }
+
+    for (shift_child child : children[positive_modulo(ni->c.z,THETA_GRID_SIZE)])
+    {
+
+      float3 child_pose = child.shift_pose + ni->pose;
+      CUDA_GEO::coord pc = float32coord(child_pose);
+//      std::cout<<pc.x<<" "<<pc.y<<std::endl;
+      nodeInfo* p = m_getNode(pc);
+
+      if (p && !p->inClosed)
+      {
+//        std::cout<<p->c.x<<" "<<p->c.y<<std::endl;
+        float new_g = child.cost + ni->g + mm_obsCostAt(pc,0);
+        if (p->g > new_g && !(!mm_isfree(pc) && reached_free_zone))
+        {
+//          std::cout<<"asdf"<<std::endl;
+          p->g = new_g;
+          p->pose = child_pose;
+          p->ptr2parent = ni;
+          p->h = dist2D(p->c,glb_tgt);
+          _PQ.insert(p,p->g);
+        }
+      }
+    }
+  }
+
+  return closest_coord;
+}
+
 void Dijkstra::hybrid_update_neighbour_with_line(const float3 &shift_pose, nodeInfo *m, float trans_cost,const CUDA_GEO::pos &seg_a, const CUDA_GEO::pos &seg_b)
 {
   float3 child_pose = shift_pose + m->pose;
