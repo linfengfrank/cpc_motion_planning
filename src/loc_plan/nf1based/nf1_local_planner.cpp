cd078796b1c9a53bf56dcb2cd6139b4db95f41a2
nyaruko <shupenglai@gmail.com>
2021-06-25 21:21:26 +0800

    Add max speed and turn off acurate reaching

--- src/loc_plan/nf1based/nf1_local_planner.cpp
+++ src/loc_plan/nf1based/nf1_local_planner.cpp
@@ -14,7 +14,7 @@ NF1LocalPlanner::NF1LocalPlanner():
   m_nf1_map(nullptr)
 {
   std::string dp_file_location;
-  float var_s, var_theta, step_dt, local_safety_radius;
+  float var_s, var_theta, step_dt, local_safety_radius, max_speed;
   int step_num;
   bool use_auto_direction;
   m_nh.param<std::string>("/dp_file_location",dp_file_location,"");
@@ -28,6 +28,7 @@ NF1LocalPlanner::NF1LocalPlanner():
   m_nh.param<int>("/batch_num",m_batch_num,40);
   m_nh.param<int>("/episode_num",m_episode_num,2);
   m_nh.param<float>("/local_safety_radius",local_safety_radius,0.401f);
+  m_nh.param<float>("/max_speed", max_speed, 1.0f);
 
   m_nf1_sub = m_nh.subscribe("/nf1",1,&NF1LocalPlanner::nf1_call_back, this);
 
@@ -46,6 +47,7 @@ NF1LocalPlanner::NF1LocalPlanner():
   m_pso_planner->m_swarm.set_var(make_float3(var_s,var_theta,1.0f));
   m_pso_planner->m_eva.m_using_auto_direction = use_auto_direction;
   m_pso_planner->m_eva.m_safety_radius = local_safety_radius;
+  m_pso_planner->m_eva.m_max_speed = max_speed;
   m_pso_planner->m_file_location = dp_file_location;
   m_pso_planner->initialize();
   m_recover_planner.init_swarm(step_num, step_dt, var_s, var_theta, dp_file_location);
@@ -366,11 +368,11 @@ void NF1LocalPlanner::cycle_init()
   if (cycle_initialized)
     return;
 
-  float2 diff = m_goal.s.p - m_carrot.p;
-  if (sqrtf(dot(diff,diff)) <= 2*m_edt_map->m_grid_step)
-    m_pso_planner->m_eva.m_accurate_reaching = true;
-  else
-    m_pso_planner->m_eva.m_accurate_reaching = false;
+//  float2 diff = m_goal.s.p - m_carrot.p;
+//  if (sqrtf(dot(diff,diff)) <= 2*m_edt_map->m_grid_step)
+//    m_pso_planner->m_eva.m_accurate_reaching = true;
+//  else
+//    m_pso_planner->m_eva.m_accurate_reaching = false;
 
   cycle_initialized = true;
   bool is_heading_ref;
