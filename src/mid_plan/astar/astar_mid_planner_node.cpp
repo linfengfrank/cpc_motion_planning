a7ad878195fcd0d636cb8e1ddef822de4b2ad53f
nyaruko <shupenglai@gmail.com>
2021-08-31 16:00:30 +0800

    change back to the most basic A* solution

--- src/mid_plan/astar/astar_mid_planner_node.cpp
+++ src/mid_plan/astar/astar_mid_planner_node.cpp
@@ -16,6 +16,7 @@
 #define USE2D
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
 PointCloud::Ptr pclOut (new PointCloud);
+ros::Publisher* line_vis_pub;
 ros::Publisher* line_pub;
 Astar *mid_map=nullptr;
 float FLY_HEIGHT;
@@ -28,7 +29,25 @@ cpc_motion_planning::ref_data ref;
 std::vector<CUDA_GEO::pos> guide_path;
 CUDA_GEO::coord glb_tgt;
 float mid_safety_radius;
-
+//---
+//---
+void publish_path(const std::vector<CUDA_GEO::coord> &path)
+{
+  //publish the point cloud to rviz for checking
+  CUDA_GEO::pos p;
+  for (int i=0; i<path.size(); i++)
+  {
+    p = mid_map->coord2pos(path[i]);
+    pcl::PointXYZRGB clrP;
+    clrP.x = p.x;
+    clrP.y = p.y;
+    clrP.z = p.z;
+    clrP.a = 255;
+    pclOut->points.push_back (clrP);
+  }
+  line_vis_pub->publish(pclOut);
+  pclOut->clear();
+}
 //---
 float actualDistBetweenCoords(const CUDA_GEO::coord &c1, const CUDA_GEO::coord &c2)
 {
@@ -58,13 +77,6 @@ float pathLength(const std::vector<CUDA_GEO::coord> &path)
     length += sqrt(static_cast<float>(shift.square()))*mid_map->getGridStep();
   }
   return length;
-}
-//---
-float cascadePath(const std::vector<CUDA_GEO::coord> &path, std::vector<CUDA_GEO::coord> &cascade, CUDA_GEO::coord goal)
-{
-  planPath(path[0], goal, cascade);
-  cascade.insert(cascade.end(),path.begin(),path.end());
-  return pathLength(cascade) + 1*actualDistBetweenCoords(cascade[0],goal);
 }
 //---
 void mapCallback(const cpc_aux_mapping::grid_map::ConstPtr& msg)
@@ -106,71 +118,17 @@ void glb_plan(const ros::TimerEvent& msg)
   glb_tgt = mid_map->rayCast(start,glb_tgt).back();
 
   // Do the planning
-  // Initialize the list
-  std::vector<std::vector<CUDA_GEO::coord>> path_list(3);
-  std::vector<float> cost_list(2);
-  std::vector<CUDA_GEO::coord> path_adopt;
+  std::vector<CUDA_GEO::coord> path;
+  float cost;
 
-  if (!received_ref)
-  {
-    // Plan A: the new plan
-    cost_list[0]=planPath(start,glb_tgt,path_list[0]);
-  }
-  else
-  {
-    // Plan B: modify based on current trajectory
-    float ini_v_x = ref.data[3];
-    float ini_v_y = ref.data[4];
-    float ini_v_z = ref.data[5];
-    float speed = sqrtf(ini_v_x*ini_v_x + ini_v_y*ini_v_y + ini_v_z*ini_v_z);
-    int max_i = max(speed/0.8f/0.05f,20);
-    //std::cout<<"----"<<max_i<<"----"<<ref.cols<<std::endl;
-    for (int i=0; i<ref.cols; i++)
-    {
-      CUDA_GEO::pos p(ref.data[i*ref.rows],ref.data[i*ref.rows+1],ref.data[i*ref.rows+2]);
-      CUDA_GEO::coord c = mid_map->pos2coord(p);
-#ifdef USE2D
-      c.z = tgt_height_coord;
-#endif
-      if (i > max_i)
-        break;
-
-      if (path_list[1].empty() || !(path_list[1].back() == c))
-      {
-        if(mid_map->isOccupied(c,0.5f) || !mid_map->isInside(c))
-          break;
-        else
-          path_list[1].push_back(c);
-      }
-    }
-
-    if (path_list[1].empty())
-    {
-      path_list[1].push_back(start);
-    }
-
-    std::reverse(path_list[1].begin(),path_list[1].end());
-    cost_list[1] = cascadePath(path_list[1],path_list[2],glb_tgt);
-  }
-
-  unsigned int selected_case = 0;
-  if(!received_ref)
-  {
-    path_adopt = path_list[0];
-    selected_case = 0;
-  }
-  else
-  {
-    selected_case = 2;
-    path_adopt = path_list[2];
-  }
+  cost =planPath(start,glb_tgt,path);
 
   //publish the guide line
-  std::reverse(path_adopt.begin(),path_adopt.end());
+  std::reverse(path.begin(),path.end());
   cpc_motion_planning::guide_line line_msg;
   geometry_msgs::Point line_pnt;
   guide_path.clear();
-  for (CUDA_GEO::coord &path_crd : path_adopt)
+  for (CUDA_GEO::coord &path_crd : path)
   {
     CUDA_GEO::pos path_pnt = mid_map->coord2pos(path_crd);
     guide_path.push_back(path_pnt);
@@ -181,11 +139,15 @@ void glb_plan(const ros::TimerEvent& msg)
   }
   line_pub->publish(line_msg);
 
+#ifdef SHOWPC
+  publish_path(path);
+#endif
+
 
   auto end_time = std::chrono::steady_clock::now();
-      std::cout << "Planning time: "
-                << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count()
-                << " ms" << std::endl;
+  std::cout << "Mid planning time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count()
+            << " ms" << std::endl;
 }
 //---
 void get_reference(const cpc_motion_planning::ref_data::ConstPtr &msg)
@@ -198,11 +160,13 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "mid_layer_node");
   line_pub = new ros::Publisher;
+  line_vis_pub = new ros::Publisher;
   ros::NodeHandle nh;
   ros::Subscriber map_sub = nh.subscribe("/edt_map", 1, &mapCallback);
   ros::Subscriber glb_tgt_sub = nh.subscribe("/move_base_simple/goal", 1, &goalCallback);
   ros::Subscriber state_sub = nh.subscribe("/ref_traj", 1, get_reference);
   *line_pub = nh.advertise<cpc_motion_planning::guide_line>("/mid_layer/guide_line",1);
+  *line_vis_pub = nh.advertise<PointCloud>("/mid_layer/guide_line_vis",1);
 
   pclOut->header.frame_id = "/world";
   nh.param<float>("/nndp_cpp/fly_height",FLY_HEIGHT,2.0);
@@ -213,6 +177,8 @@ int main(int argc, char **argv)
 
   ros::spin();
 
+  delete line_pub;
+  delete line_vis_pub;
   if (mid_map)
   {
     delete mid_map;
